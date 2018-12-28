8e777a69d4ce547cbe50feb80075cdccdc894175
Molly McCarron <mollymccarron@Mollys-Air.fios-router.home>
2018-12-28 10:20:33 -0500

    Listing episodes

--- lib/codenewbie/cli.rb
+++ lib/codenewbie/cli.rb
@@ -3,8 +3,12 @@ Our CLI Controller
 class Codenewbie::CLI
 
   def call
-    puts "List of Codenewbie Episodes! ummmm"
+    puts "List of Codenewbie Episodes!"
+    list_episodes
   end
 
+  def list_episodes
+    puts "1. A 2. B"
+  end
 
-#end
+end
